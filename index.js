1329abfb5ad85f1b6b260d98fb060d374f3fbd9f
SamVarejao <scvarejao@gmail.com>
2021-05-03 19:00:54 +0100

    Updated dependacies

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.use("/", require("./routes"));
 
 //--------------------------*/
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up on port ${port}.`));
